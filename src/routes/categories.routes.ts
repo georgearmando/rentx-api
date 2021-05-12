176174775fd9f0190aef8f261dd81c9e8a91ab86
George Armando <georgearmando54@gmail.com>
2021-05-12 14:56:43 +0100

    Initial commit

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,27 @@
+import { response, Router } from 'express';
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
+
+const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
+
+categoriesRoutes.post('/', (request, response) => {
+  const { name, description } = request.body;
+
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ error: 'Category already exists!' });
+  }
+
+  categoriesRepository.create({ name, description });
+
+  return response.status(201).send();
+})
+
+categoriesRoutes.get('/', (request, response) => {
+  const allCategories = categoriesRepository.list();
+
+  return response.status(200).json(allCategories);
+});
+
+export { categoriesRoutes };
