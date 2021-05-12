50fd0c552b5d83d2819c400d93ca1fe8a1812cf7
George Armando <georgearmando54@gmail.com>
2021-05-12 17:27:51 +0100

    feat: add create category service

---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,23 @@
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateCategoryService {
+  constructor(private categoriesRepository: CategoriesRepository) { }
+
+  execute({ name, description }: IRequest) {
+    const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+
+    if (categoryAlreadyExists) {
+      throw new Error('Category already exists!');
+    }
+
+    this.categoriesRepository.create({ name, description });
+  }
+}
+
+export { CreateCategoryService }
