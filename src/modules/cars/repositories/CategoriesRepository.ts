10cea9b20a60dc849f1d74562cffdd3608f8f6df
George Armando <georgearmando54@gmail.com>
2021-05-15 00:57:28 +0100

    chore: inserts the category model, category repository and category service into their respective folders

---
+++ src/modules/cars/repositories/CategoriesRepository.ts
@@ -0,0 +1,34 @@
+import { Category } from "../models/Category";
+import { ICategoryRepository, ICreateCategoryDTO } from "./ICategoryRepository";
+
+class CategoriesRepository implements ICategoryRepository {
+  private categories: Category[]
+
+  constructor() {
+    this.categories = [];
+  }
+
+  create({ name, description }: ICreateCategoryDTO): void {
+    const category = new Category();
+
+    Object.assign(category, {
+      name,
+      description,
+      created_at: new Date()
+    });
+
+    this.categories.push(category);
+  }
+
+  list(): Category[] {
+    return this.categories;
+  }
+
+  findByName(name: string): Category {
+    const category = this.categories.find(category => category.name === name);
+
+    return category;
+  }
+}
+
+export { CategoriesRepository };
---
+++ src/modules/cars/services/CreateCategoryService.ts
@@ -0,0 +1,22 @@
+import { ICategoryRepository } from "../repositories/ICategoryRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateCategoryService {
+  constructor(private categoriesRepository: ICategoryRepository) { }
+
+  execute({ name, description }: IRequest) {
+    const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+
+    if (categoryAlreadyExists) {
+      throw new Error('Category already exists!');
+    }
+
+    this.categoriesRepository.create({ name, description });
+  }
+}
+
+export { CreateCategoryService }
