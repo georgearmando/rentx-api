5d7335c826f0117707eda3ffee651605af3259bf
George Armando <georgearmando54@gmail.com>
2021-05-15 01:13:08 +0100

    feat: Adds the route to the specifications

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.json());
 
 app.use('/categories', categoriesRoutes);
+app.use('/specifications', specificationsRoutes)
 
 app.listen(3333, () => {
   console.log('Server is running on port 3333!')
