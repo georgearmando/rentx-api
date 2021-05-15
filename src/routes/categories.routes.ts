ba01215fbabd5307f4206b677f016dac98fd14a5
George Armando <georgearmando54@gmail.com>
2021-05-15 12:00:48 +0100

    refactor: Replaces the category creation logic in the post method

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -6,13 +6,7 @@ const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (request, response) => {
-  const { name, description } = request.body;
-
-  const createCategoryService = new CreateCategoryService(categoriesRepository);
-
-  createCategoryService.execute({ name, description })
-
-  return response.status(201).send();
+  return createCategoryController.handle(request, response);
 })
 
 categoriesRoutes.get('/', (request, response) => {
