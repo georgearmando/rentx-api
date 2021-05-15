8521ee6c72cd097f9feb91bfd51561794b6533b6
George Armando <georgearmando54@gmail.com>
2021-05-15 13:57:35 +0100

    refactor: Replaces the list categories logic in the get method

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,15 @@
-import { response, Router } from 'express';
-import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
+import { Router } from 'express';
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { listCategoriesController } from '../modules/cars/useCases/listCategory';
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (request, response) => {
   return createCategoryController.handle(request, response);
 })
 
 categoriesRoutes.get('/', (request, response) => {
-  const allCategories = categoriesRepository.list();
-
-  return response.status(200).json(allCategories);
+  return listCategoriesController.handle(request, response);
 });
 
 export { categoriesRoutes };
