e0bf16c5da43a8a065efa6b72928c4f6eeb55c27
George Armando <georgearmando54@gmail.com>
2021-05-15 01:11:16 +0100

    refactor: Adds the import of specificatios.routes

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { categoriesRoutes } from './routes/categories.routes';
+import { specificationsRoutes } from './routes/specifications.routes';
 
 const app = express();
 
