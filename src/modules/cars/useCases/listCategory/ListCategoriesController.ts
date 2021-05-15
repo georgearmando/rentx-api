c018f898f9581b4eecea565b824196f1af34032c
George Armando <georgearmando54@gmail.com>
2021-05-15 13:49:28 +0100

    refactor: Adds the controller for list categories

---
+++ src/modules/cars/useCases/listCategory/ListCategoriesController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
+
+class ListCategoriesController {
+  constructor(private listCategoriesUseCase: ListCategoriesUseCase) { }
+
+  handle(request: Request, response: Response): Response {
+    const allCategories = this.listCategoriesUseCase.execute();
+
+    return response.status(200).json(allCategories);
+  }
+}
+
+export { ListCategoriesController }
