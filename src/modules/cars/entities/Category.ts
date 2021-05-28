a314849e80345bd8392122f47a38f031d006f8f4
George Armando <georgearmando54@gmail.com>
2021-05-29 00:40:47 +0100

    feat: Creates the category migration and refactor the entity

---
+++ src/modules/cars/entities/Category.ts
@@ -0,0 +1,25 @@
+import { v4 as uuid } from 'uuid';
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm'
+
+@Entity('categories')
+class Category {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
+
+export { Category }
