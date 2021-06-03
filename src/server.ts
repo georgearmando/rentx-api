9e58d7986c49ad0d5daf01a6c483683fbcee5c9b
George Armando <georgearmando54@gmail.com>
2021-06-03 11:49:27 +0100

    feat: Import the container on the server

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import swaggerUI from 'swagger-ui-express';
 
 import './database';
+import './shared/container';
 import { routes } from './routes';
 import swaggerFile from './swagger.json';
 
