cccd4dc984afbdfca74ce4824cd83ff5659f761d
George Armando <georgearmando54@gmail.com>
2021-06-07 13:38:04 +0100

    feat: Add user route to index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { categoriesRoutes } from './categories.routes';
 import { specificationsRoutes } from './specifications.routes';
+import { userRoutes } from './users.routes';
 
 const routes = Router();
 
 routes.use('/categories', categoriesRoutes);
 routes.use('/specifications', specificationsRoutes);
+routes.use('/users', userRoutes);
 
 export { routes }
