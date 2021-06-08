e486dee9d860b01879c0ac442cc5e7e3198a41e7
George Armando <georgearmando54@gmail.com>
2021-06-08 14:13:38 +0100

    refactor: Checks for the existence of a user

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -11,14 +11,18 @@ class CreateUserUseCase {
   ) { }
 
   async execute({ name, email, driver_license, password }: ICreateUserDTO): Promise<void> {
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error('User already exists!');
+    }
+
     const passwordHash = await hash(password, 8);
 
     await this.usersRepository.create({
       name,
       email,
       driver_license,
-      password
-    });
       password: passwordHash
     });
   }
