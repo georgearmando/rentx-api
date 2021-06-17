7d5cba9e103927d87905af19c272040017f44236
George Armando <georgearmando54@gmail.com>
2021-06-17 23:57:04 +0100

    test: Tests category creation functionality

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -1,8 +1,48 @@
-describe('Criar categoria', () => {
-  it('Espero que 2 + 2 seja 4', () => {
-    const soma = 2 + 2;
-    const resultado = 4;
+import { AppError } from "../../../../errors/AppErrors";
+import { CategoriesRepositoryInMemory } from "../../repositories/in-memory/CategoriesRepositoryInMemory";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
-    expect(soma).toBe(resultado)
-  })
-})
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
+let createCategoryUseCase: CreateCategoryUseCase;
+
+describe('Create Category', () => {
+  beforeEach(() => {
+    categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+    createCategoryUseCase = new CreateCategoryUseCase(categoriesRepositoryInMemory);
+  });
+
+  it('Should be able to create a new category', async () => {
+    const category = {
+      name: 'Category sample',
+      description: 'Description sample'
+    }
+
+    await createCategoryUseCase.execute({
+      name: category.name,
+      description: category.description
+    });
+
+    const categoryCreated = await categoriesRepositoryInMemory.findByName(category.name);
+
+    expect(categoryCreated).toHaveProperty('id');
+  });
+
+  it('Should not be able to create a new category with same name from another', () => {
+    expect(async () => {
+      const category = {
+        name: 'Category sample',
+        description: 'Description sample'
+      }
+
+      await createCategoryUseCase.execute({
+        name: category.name,
+        description: category.description
+      });
+
+      await createCategoryUseCase.execute({
+        name: category.name,
+        description: category.description
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+});
