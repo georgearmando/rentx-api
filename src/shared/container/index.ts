80c11176db158ed7d53bbea777f49425617838db
George Armando <georgearmando54@gmail.com>
2021-07-06 14:51:20 +0100

    feat: add record to be injected

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -6,6 +6,8 @@ import { ICategoriesRepository } from '@modules/cars/repositories/ICategoriesRep
 import { CategoriesRepository } from '@modules/cars/infra/typeorm/repositories/CategoriesRepository';
 import { SpecificationsRepository } from '@modules/cars/infra/typeorm/repositories/SpecificationsRepository';
 import { ISpecificationsRepository } from '@modules/cars/repositories/ISpecificationsRepository';
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+import { CarsRepository } from '@modules/cars/infra/typeorm/repositories/CarsRepository';
 
 container.registerSingleton<ICategoriesRepository>(
   "CategoriesRepository", CategoriesRepository
@@ -17,4 +19,8 @@ container.registerSingleton<ISpecificationsRepository>(
 
 container.registerSingleton<IUsersRepository>(
   'UsersRepository', UsersRepository
+);
+
+container.registerSingleton<ICarsRepository>(
+  'CarsRepository', CarsRepository
 );
