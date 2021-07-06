e82628388ae1dc0019cd4f0a566a43cd3e5dcc24
George Armando <georgearmando54@gmail.com>
2021-07-06 16:39:45 +0100

    feat: add authentication to route

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,10 +1,12 @@
 import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController';
 import { Router } from 'express';
+import { ensureAdmin } from '../middlewares/ensureAdmin';
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
 
-carsRoutes.post('/', createCarController.handle);
+carsRoutes.post('/', ensureAuthenticated, ensureAdmin, createCarController.handle);
 
 export { carsRoutes }
