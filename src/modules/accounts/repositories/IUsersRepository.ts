6c77293ece7c5feec169fa0af0a472cf86e357f7
George Armando <georgearmando54@gmail.com>
2021-07-06 12:48:27 +0100

    style: remove space

--- src/modules/accounts/repositories/IUsersRepository.ts
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -1,7 +1,6 @@
 import { ICreateUserDTO } from "../dtos/ICreateUserDTO";
 import { User } from "../infra/typeorm/entities/User";
 
-
 interface IUsersRepository {
   create(data: ICreateUserDTO): Promise<void>;
   findByEmail(email: string): Promise<User>;
