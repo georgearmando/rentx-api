1614e3e69c270d1f27f80eb06bb0d3881a371380
George Armando <georgearmando54@gmail.com>
2021-07-06 12:53:06 +0100

    test: create car creation test file

---
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -0,0 +1,67 @@
+import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory";
+import { AppError } from "@shared/errors/AppErrors";
+import { CreateCarUseCase } from "./CreateCarUseCase";
+
+let createCarUseCase: CreateCarUseCase;
+let carsRepositoryInMemory: CarsRepositoryInMemory;
+
+describe('Create Car', () => {
+  beforeEach(() => {
+    carsRepositoryInMemory = new CarsRepositoryInMemory()
+    createCarUseCase = new CreateCarUseCase(carsRepositoryInMemory);
+  });
+
+  it('should be able to create a new car', async () => {
+    const car = await createCarUseCase.execute({
+      name: 'Name Car',
+      description: 'Description Car',
+      daily_rate: 100,
+      license_plate: 'ABC-123',
+      fine_amount: 60,
+      brand: 'Brand',
+      category_id: 'Category'
+    });
+
+    expect(car).toHaveProperty('id');
+  });
+
+  it('should not be able to create a car with exists license_plate', () => {
+    expect(async () => {
+      await createCarUseCase.execute({
+        name: 'Car 1',
+        description: 'Description Car',
+        daily_rate: 100,
+        license_plate: 'ABC-123',
+        fine_amount: 60,
+        brand: 'Brand',
+        category_id: 'Category'
+      })
+
+      await createCarUseCase.execute({
+        name: 'Car 2',
+        description: 'Description Car',
+        daily_rate: 100,
+        license_plate: 'ABC-123',
+        fine_amount: 60,
+        brand: 'Brand',
+        category_id: 'Category'
+      })
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should be able to create a car with available true by default', async () => {
+    const car = await createCarUseCase.execute({
+      name: 'Car Available',
+      description: 'Description Car',
+      daily_rate: 100,
+      license_plate: 'ABC-123',
+      fine_amount: 60,
+      brand: 'Brand',
+      category_id: 'Category'
+    });
+
+    console.log(car)
+
+    expect(car.available).toBe(true);
+  });
+})
