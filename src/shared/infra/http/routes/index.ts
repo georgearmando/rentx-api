2b582754bcaec7bc8688703ff1bf3ea09c1955f4
George Armando <georgearmando54@gmail.com>
2021-07-06 14:52:35 +0100

    feat: add the route for cars

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { authenticateRoutes } from './authenticate.routes';
+import { carsRoutes } from './cars.routes';
 import { categoriesRoutes } from './categories.routes';
 import { specificationsRoutes } from './specifications.routes';
 import { userRoutes } from './users.routes';
@@ -9,6 +10,7 @@ const routes = Router();
 routes.use('/categories', categoriesRoutes);
 routes.use('/specifications', specificationsRoutes);
 routes.use('/users', userRoutes);
+routes.use('/cars', carsRoutes);
 routes.use(authenticateRoutes);
 
 export { routes }
