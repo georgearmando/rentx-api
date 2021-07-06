cd61f26f73cac2fb1142ae3c2b8463c8a269169e
George Armando <georgearmando54@gmail.com>
2021-07-06 12:52:14 +0100

    test: creates a fake repository for the car

---
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -0,0 +1,39 @@
+import { ICreateCarDto } from "@modules/cars/dtos/ICreateCarDTO";
+import { Car } from "@modules/cars/infra/typeorm/entities/Cars";
+import { ICarsRepository } from "../ICarsRepository";
+
+class CarsRepositoryInMemory implements ICarsRepository {
+  cars: Car[] = [];
+
+  async create({
+    name,
+    description,
+    daily_rate,
+    license_plate,
+    fine_amount,
+    category_id,
+    brand
+  }: ICreateCarDto): Promise<Car> {
+    const car = new Car();
+
+    Object.assign(car, {
+      name,
+      description,
+      daily_rate,
+      license_plate,
+      fine_amount,
+      category_id,
+      brand
+    });
+
+    this.cars.push(car);
+
+    return car;
+  }
+
+  async findByLicensePlate(license_plate: string): Promise<Car> {
+    return this.cars.find(car => car.license_plate === license_plate);
+  }
+}
+
+export { CarsRepositoryInMemory }
