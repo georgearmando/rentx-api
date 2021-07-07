cbcf20c628a483e440dfc293a17d7b4b86813ef2
George Armando <georgearmando54@gmail.com>
2021-07-07 23:10:58 +0100

    feta: create car listing controller

---
+++ src/modules/cars/useCases/listCar/ListCarsController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+import { ListCarsUseCase } from "./ListCarsUseCase";
+
+class ListCarsController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { brand, name, category_id } = request.query;
+
+    const listCarsUseCase = container.resolve(ListCarsUseCase);
+
+    const cars = await listCarsUseCase.execute({
+      brand: brand as string,
+      name: name as string,
+      category_id: category_id as string,
+    });
+
+    return response.json(cars);
+  }
+}
+
+export { ListCarsController }
