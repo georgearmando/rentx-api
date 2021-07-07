71ac1a144309cd169d482a8e073f7c6092b8e97a
George Armando <georgearmando54@gmail.com>
2021-07-07 23:14:00 +0100

    feat: route to car listing

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,12 +1,17 @@
-import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController';
 import { Router } from 'express';
+
+import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController';
+import { ListCarsController } from '@modules/cars/useCases/listCar/ListCarsController';
 import { ensureAdmin } from '../middlewares/ensureAdmin';
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
+const listCarsController = new ListCarsController();
 
 carsRoutes.post('/', ensureAuthenticated, ensureAdmin, createCarController.handle);
+
+carsRoutes.get('/', listCarsController.handle);
 
 export { carsRoutes }
