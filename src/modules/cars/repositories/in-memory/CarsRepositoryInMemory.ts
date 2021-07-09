aad01ce3140e10a3079f6b11c7935ec4154da5a5
George Armando <georgearmando54@gmail.com>
2021-07-09 15:16:31 +0100

    feat: implement a method

--- src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -44,6 +44,10 @@ class CarsRepositoryInMemory implements ICarsRepository {
 
     return availableCars;
   }
+
+  async findById(id: string): Promise<Car> {
+    return this.cars.find(car => car.id === id);
+  }
 }
 
 export { CarsRepositoryInMemory }
