28a49851eb20ae3eec2f35830da936279465df5d
George Armando <georgearmando54@gmail.com>
2021-07-16 18:20:27 +0100

    refactor: leaves the server file with the sole responsibility of starting the server

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,37 +1,4 @@
-import "reflect-metadata";
-import express, { Request, Response, NextFunction } from 'express';
-import 'express-async-errors';
-import swaggerUI from 'swagger-ui-express';
-
-import '@shared/container';
-import createConnection from '@shared/infra/typeorm';
-import { routes } from './routes';
-import swaggerFile from '../../../swagger.json';
-import { AppError } from '@shared/errors/AppErrors';
-
-createConnection();
-
-const app = express();
-
-app.use(express.json());
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
-app.use(routes);
-
-app.use(
-  (err: Error, request: Request, response: Response, next: NextFunction) => {
-    if (err instanceof AppError) {
-      return response.status(err.statusCode).json({
-        status: 'error',
-        message: err.message,
-      });
-    }
-
-    return response.status(500).json({
-      status: 'error',
-      message: `Internal server error - ${err.message}`,
-    });
-  },
-);
+import { app } from "./app";
 
 app.listen(3333, () => {
   console.log('Server is running on port 3333!')
