720eddb91826a68086c4cf81e7aaa4bd2f1269aa
George Armando <georgearmando54@gmail.com>
2021-07-16 18:18:43 +0100

    feat: create app file

---
+++ src/shared/infra/http/app.ts
@@ -0,0 +1,36 @@
+import "reflect-metadata";
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+import swaggerUI from 'swagger-ui-express';
+
+import '@shared/container';
+import createConnection from '@shared/infra/typeorm';
+import { routes } from './routes';
+import swaggerFile from '../../../swagger.json';
+import { AppError } from '@shared/errors/AppErrors';
+
+createConnection();
+
+const app = express();
+
+app.use(express.json());
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
+app.use(routes);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: 'error',
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: `Internal server error - ${err.message}`,
+    });
+  },
+);
+
+export { app }
