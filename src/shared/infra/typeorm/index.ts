b1961c746c34c620a9f3b27949aa3eaa76f9c332
George Armando <georgearmando54@gmail.com>
2021-07-22 18:47:03 +0100

    fix: add missing host parameter

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,10 +1,13 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-export default async (): Promise<Connection> => {
+// recebe o host
+export default async (host = 'database'): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultOptions, {
+      // faz a condição
+      host: process.env.NODE_ENV === 'test' ? 'localhost' : host,
       database:
         process.env.NODE_ENV === "test"
           ? "rentx_test"
