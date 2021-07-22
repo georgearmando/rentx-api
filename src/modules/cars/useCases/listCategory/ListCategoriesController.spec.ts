f2e524c79b0f1a8b1c05753348f7bf23407f45ba
George Armando <georgearmando54@gmail.com>
2021-07-22 22:42:50 +0100

    test: adds integration testing to category listing

---
+++ src/modules/cars/useCases/listCategory/ListCategoriesController.spec.ts
@@ -0,0 +1,51 @@
+import { Connection } from 'typeorm';
+import request from 'supertest'
+import { v4 as uuid } from 'uuid';
+import { hash } from 'bcryptjs';
+
+import { app } from '@shared/infra/http/app';
+import createConnection from '@shared/infra/typeorm';
+
+let connection: Connection;
+
+describe('List Categories', () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+
+    const id = uuid();
+    const password = await hash('admin', 8);
+
+    await connection.query(
+      `INSERT INTO USERS(id, name, email, password, "isAdmin", created_at, driver_license )
+        values('${id}', 'admin', 'admin@rentx.com.br', '${password}', true, 'now()', 'XXXXXX')
+      `
+    );
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it('Should be able to list all categories', async () => {
+    const responseToken = await request(app).post('/sessions').send({
+      email: 'admin@rentx.com.br',
+      password: 'admin'
+    });
+
+    const { token } = responseToken.body;
+
+    await request(app).post('/categories').send({
+      name: 'Category Name',
+      description: 'Category Description'
+    }).set({
+      Authorization: `Bearer ${token}`
+    });
+
+    const response = await request(app).get('/categories');
+
+    expect(response.status).toBe(200);
+    expect(response.body.length).toBe(1);
+  });
+});
