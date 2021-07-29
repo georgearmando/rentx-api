b9d48ef49507e3a6a2eeff5d236f9f8ca0c51e79
George Armando <georgearmando54@gmail.com>
2021-07-29 15:37:56 +0100

    feat: add route for listing rentals by user

--- src/shared/infra/http/routes/rentals.routes.ts
+++ src/shared/infra/http/routes/rentals.routes.ts
@@ -1,14 +1,22 @@
 import { Router } from 'express';
 import { CreateRentalController } from '@modules/rentals/useCases/createRental/CreateRentalController';
 import { DevolutionRentalController } from '@modules/rentals/useCases/devolutionRental/DevolutionRentalController';
+import { ListRentalsByUserController } from '@modules/rentals/useCases/listRentalsByUser/ListRentalsByUseController'
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
 const rentalsRoutes = Router();
 
 const createRentalController = new CreateRentalController();
 const devolutionRentalController = new DevolutionRentalController();
+const listRentalsByUserController = new ListRentalsByUserController();
 
 rentalsRoutes.post('/', ensureAuthenticated, createRentalController.handle);
 rentalsRoutes.post('/devolution/:id', ensureAuthenticated, devolutionRentalController.handle);
+
+rentalsRoutes.get(
+  "/user",
+  ensureAuthenticated,
+  listRentalsByUserController.handle
+);
 
 export { rentalsRoutes }
