3c4021f93335f3a3ea3342ec054b2f3398302508
George Armando <georgearmando54@gmail.com>
2021-07-30 22:43:52 +0100

    feat: register authentication provider dependency

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,7 +1,13 @@
 import { container } from "tsyringe";
+import { IAuthProvider } from "./AuthProvider/IAuthProvider";
+import { JWTAuthProvider } from "./AuthProvider/implementations/JWTAuthProvider";
 import { IDateProvider } from "./DateProvider/IDateProvider";
 import { DayjsDateProvider } from "./DateProvider/implementations/DayjsDateProvider";
 
 container.registerSingleton<IDateProvider>(
   'DateProvider', DayjsDateProvider
+);
+
+container.registerSingleton<IAuthProvider>(
+  'AuthProvider', JWTAuthProvider
 );
