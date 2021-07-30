ce630cc1b8af8722b26f4eb22be713e541275e7e
George Armando <georgearmando54@gmail.com>
2021-07-31 00:03:54 +0100

    feat: define methods for auth provider

--- src/shared/container/providers/AuthProvider/IAuthProvider.ts
+++ src/shared/container/providers/AuthProvider/IAuthProvider.ts
@@ -1,5 +1,7 @@
 interface IAuthProvider {
   generateToken(user_id: string): string;
+  generateRefreshToken(email: string, user_id: string): string;
+  refreshTokenExpireDate(): Date;
   verifyToken(token: string): string;
 }
 
