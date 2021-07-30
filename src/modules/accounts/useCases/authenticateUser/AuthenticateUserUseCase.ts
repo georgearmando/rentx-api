4fdec370efb8509dc3f9180d125b85f888f15a4e
George Armando <georgearmando54@gmail.com>
2021-07-31 00:02:38 +0100

    feat: add refresh token to authentication

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -4,6 +4,8 @@ import { inject, injectable } from "tsyringe"
 import { AppError } from "@shared/errors/AppErrors";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 import { IAuthProvider } from "@shared/container/providers/AuthProvider/IAuthProvider";
+import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
+import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
 
 interface IRequest {
   email: string;
@@ -16,6 +18,7 @@ interface IResponse {
     email: string;
   };
   token: string;
+  refresh_token: string;
 }
 
 @injectable()
@@ -25,7 +28,10 @@ class AuthenticateUserUseCase {
     private usersRepository: IUsersRepository,
 
     @inject('AuthProvider')
-    private authProvider: IAuthProvider
+    private authProvider: IAuthProvider,
+
+    @inject('UsersTokensRepository')
+    private usersTokensRepository: IUsersTokensRepository,
   ) { }
 
   async execute({ email, password }: IRequest): Promise<IResponse> {
@@ -43,8 +49,19 @@ class AuthenticateUserUseCase {
 
     const token = this.authProvider.generateToken(user.id);
 
+    const refresh_token = this.authProvider.generateRefreshToken(email, user.id);
+
+    const refresh_token_expires_date = this.authProvider.refreshTokenExpireDate();
+
+    await this.usersTokensRepository.create({
+      user_id: user.id,
+      refresh_token,
+      expires_date: refresh_token_expires_date
+    });
+
     const tokenReturn: IResponse = {
       token,
+      refresh_token,
       user: {
         name: user.name,
         email: user.email
