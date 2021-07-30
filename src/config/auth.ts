c034b114d919d2dcd41c4f136825a5057f374705
George Armando <georgearmando54@gmail.com>
2021-07-31 00:01:36 +0100

    feat: add properties to jwt

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,9 @@
 export default {
   jwt: {
-    secret: 'c3686ca82c5b4aad5312da8d237edd9f',
-    expiresIn: '1d',
+    secret_token: 'c3686ca82c5b4aad5312da8d237edd9f',
+    expiresIn: '15m',
+    secret_refresh_token: '20debe31aa9bfe69602239d1fc5e974a',
+    expires_in_refresh_token: '30d',
+    expires_refresh_token_days: 30,
   },
 };
