9c5af67bf7dd54913b0ee83e52c303c3dd40f907
George Armando <georgearmando54@gmail.com>
2021-11-04 09:35:27 +0100

    feat(RefreshToken): add /password endpoint

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { authenticateRoutes } from './authenticate.routes';
 import { carsRoutes } from './cars.routes';
 import { categoriesRoutes } from './categories.routes';
+import { passwordRoutes } from './password.routes';
 import { rentalsRoutes } from './rentals.routes';
 import { specificationsRoutes } from './specifications.routes';
 import { userRoutes } from './users.routes';
@@ -12,7 +13,8 @@ routes.use('/categories', categoriesRoutes);
 routes.use('/specifications', specificationsRoutes);
 routes.use('/users', userRoutes);
 routes.use('/cars', carsRoutes);
-routes.use('/rentals', rentalsRoutes)
+routes.use('/rentals', rentalsRoutes);
+routes.use('/password', passwordRoutes);
 routes.use(authenticateRoutes);
 
 export { routes }
