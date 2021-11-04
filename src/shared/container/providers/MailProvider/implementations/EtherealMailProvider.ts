f0a1c75c834036f255d2532367e8b3204648b8c4
George Armando <georgearmando54@gmail.com>
2021-11-04 09:32:19 +0100

    feat: add Ethereal mail provider

---
+++ src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -0,0 +1,49 @@
+import { injectable } from "tsyringe";
+import nodemailer, { Transporter } from 'nodemailer';
+import handlebars from 'handlebars';
+import fs from 'fs';
+
+import { IMailProvider } from "../IMailProvider";
+
+@injectable()
+class EtherealMailProvider implements IMailProvider {
+  private client: Transporter;
+  constructor() {
+    nodemailer
+      .createTestAccount()
+      .then((account) => {
+        const transporter = nodemailer.createTransport({
+          host: account.smtp.host,
+          port: account.smtp.port,
+          secure: account.smtp.secure,
+          auth: {
+            user: account.user,
+            pass: account.pass,
+          },
+        });
+
+        this.client = transporter;
+      })
+      .catch((err) => console.error(err));
+  }
+
+  async sendMail(to: string, subject: string, variables: any, path: string): Promise<void> {
+    const templateFileContent = fs.readFileSync(path).toString('utf-8');
+
+    const templateParse = handlebars.compile(templateFileContent);
+
+    const templateHTML = templateParse(variables);
+
+    const message = await this.client.sendMail({
+      to,
+      from: 'Rentx <noreplay@rentx.com>',
+      subject,
+      html: templateHTML,
+    });
+
+    console.log('Message sent: %s', message.messageID);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message));
+  }
+}
+
+export { EtherealMailProvider }
