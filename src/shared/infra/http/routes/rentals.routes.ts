fc6ad798e78bcf9a5f9a2f1cff6aed2bfa39dc47
George Armando <georgearmando54@gmail.com>
2021-11-06 03:26:29 +0100

    fix: use the correct import class

--- src/shared/infra/http/routes/rentals.routes.ts
+++ src/shared/infra/http/routes/rentals.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { CreateRentalController } from '@modules/rentals/useCases/createRental/CreateRentalController';
 import { DevolutionRentalController } from '@modules/rentals/useCases/devolutionRental/DevolutionRentalController';
-import { ListRentalsByUserController } from '@modules/rentals/useCases/listRentalsByUser/ListRentalsByUseController'
+import { ListRentalsByUserController } from '@modules/rentals/useCases/listRentalsByUser/ListRentalsByUserController'
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
 const rentalsRoutes = Router();
