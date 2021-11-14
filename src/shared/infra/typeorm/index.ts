b31a6b5bffd5434f26c4b5d811b26f774027d818
George Armando <georgearmando54@gmail.com>
2021-11-14 21:34:33 +0100

    fix: remove host

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,13 +1,11 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
 // recebe o host
-export default async (host = 'database'): Promise<Connection> => {
+export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultOptions, {
-      // faz a condição
-      host: process.env.NODE_ENV === 'test' ? 'localhost' : host,
       database:
         process.env.NODE_ENV === "test"
           ? "rentx_test"
