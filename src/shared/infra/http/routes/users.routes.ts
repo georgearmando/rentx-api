2fe2b74ed046af6cfa97f20953633b416129426c
George Armando <georgearmando54@gmail.com>
2021-11-14 02:08:29 +0100

    feat: add user profile endpoint

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -5,12 +5,14 @@ import uploadConfig from '@config/upload';
 import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
 import { CreateUserController } from "@modules/accounts/useCases/createUser/CreateUserController";
 import { UpdateUserAvatarController } from "@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
+import { UserProfileController } from "@modules/accounts/useCases/userProfile/UserProfileController";
 
 const userRoutes = Router();
-const uploadAvatar = multer(uploadConfig.upload('./tmp/avatar'));
+const uploadAvatar = multer(uploadConfig);
 
 const createUserController = new CreateUserController();
 const updateUserAvatarController = new UpdateUserAvatarController();
+const userProfileController = new UserProfileController();
 
 userRoutes.post('/', createUserController.handle);
 userRoutes.patch(
@@ -19,5 +21,6 @@ userRoutes.patch(
   uploadAvatar.single('avatar'),
   updateUserAvatarController.handle
 );
+userRoutes.get('/profile', ensureAuthenticated, userProfileController.handle);
 
 export { userRoutes }
