1b4edf33a8ca3d5b1049313e57afbc9b72cb1964
George Armando <georgearmando54@gmail.com>
2021-11-14 01:58:13 +0100

    feat: implements gmail provider

---
+++ src/shared/container/providers/MailProvider/implementations/GmailMailProvider.ts
@@ -0,0 +1,44 @@
+import { SES } from "aws-sdk";
+import fs from "fs";
+import handlebars from "handlebars";
+import nodemailer, { Transporter } from "nodemailer";
+import { injectable } from "tsyringe";
+
+import { IMailProvider } from "../IMailProvider";
+
+@injectable()
+class GmailMailProvider implements IMailProvider {
+  private client: Transporter;
+
+  constructor() {
+    this.client = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.GMAIL_EMAIL,
+        pass: process.env.GMAIL_PASS,
+      }
+    });
+  }
+
+  async sendMail(
+    to: string,
+    subject: string,
+    variables: any,
+    path: string
+  ): Promise<void> {
+    const templateFileContent = fs.readFileSync(path).toString("utf-8");
+
+    const templateParse = handlebars.compile(templateFileContent);
+
+    const templateHTML = templateParse(variables);
+
+    await this.client.sendMail({
+      to,
+      from: "Rentx <georgearmando54@gmail.com>",
+      subject,
+      html: templateHTML,
+    });
+  }
+}
+
+export { GmailMailProvider };
