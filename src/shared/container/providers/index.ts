104ffccffe2e4a3d912371db4d70290412cb0615
George Armando <georgearmando54@gmail.com>
2021-11-14 02:02:56 +0100

    refactor: imports all providers

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,19 +1,4 @@
-import { container } from "tsyringe";
-import { IAuthProvider } from "./AuthProvider/IAuthProvider";
-import { JWTAuthProvider } from "./AuthProvider/implementations/JWTAuthProvider";
-import { IDateProvider } from "./DateProvider/IDateProvider";
-import { DayjsDateProvider } from "./DateProvider/implementations/DayjsDateProvider";
-import { IMailProvider } from "./MailProvider/IMailProvider";
-import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMailProvider";
-
-container.registerSingleton<IDateProvider>(
-  'DateProvider', DayjsDateProvider
-);
-
-container.registerSingleton<IAuthProvider>(
-  'AuthProvider', JWTAuthProvider
-);
-
-container.registerInstance<IMailProvider>(
-  'MailProvider', new EtherealMailProvider()
-);
+import "./DateProvider";
+import "./MailProvider";
+import "./StorageProvider";
+import "./AuthProvider";
