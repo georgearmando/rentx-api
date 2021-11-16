6dbe8f927bfc1457e4d6910a0e2c9c04c80e382f
George Armando <georgearmando54@gmail.com>
2021-11-16 14:16:21 +0100

    feat: implements sentry

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,6 +1,8 @@
 import "reflect-metadata";
 import "dotenv/config";
 import express, { Request, Response, NextFunction } from 'express';
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 import 'express-async-errors';
 import swaggerUI from 'swagger-ui-express';
 import cors from 'cors';
@@ -18,6 +20,19 @@ createConnection();
 const app = express();
 
 app.use(rateLimiter);
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use(express.json());
 app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
 app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
@@ -25,6 +40,8 @@ app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 app.use(cors());
 app.use(routes);
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
